523b71db2929ee422bbb65c3b4fe95cc03e399f9
Antonio <antoniocutri841@gmail.com>
2025-05-20 23:36:34 +0200

    create table migration and model

---
+++ app/Models/table.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class table extends Model
+{
+    /** @use HasFactory<\Database\Factories\TableFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'restaurant_id',
+        'table_number',
+        'seats',
+    ];
+
+    public function restaurant() {
+        return $this->belongsTo(Restaurant::class);
+    }
+
+}
