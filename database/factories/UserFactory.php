87f9a87a1446acd64834fdac97bedab28e1f8a8b
Antonio <antoniocutri841@gmail.com>
2025-05-22 22:38:41 +0200

    updated user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'role' => fake()->randomElement(['customer', 'admin']),
+            'role' => fake()->randomElement(['customer', 'owner', 'admin']),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
