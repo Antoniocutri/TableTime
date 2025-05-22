2b0737418a61bf04fdcf4ce3df4c3fd70d74cff1
Antonio <antoniocutri841@gmail.com>
2025-05-22 23:28:51 +0200

    updated database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(20)->create();
+        $this->call(RestaurantSeeder::class);
+        $this->call(ReviewSeeder::class);
+        $this->call(TableSeeder::class);
+        $this->call(RestaurantScheduleSeeder::class);
     }
 }
