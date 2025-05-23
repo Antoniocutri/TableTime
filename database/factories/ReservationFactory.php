f1d35728830032c2326a4a4ffe478cf0bd0526c6
Antonio <antoniocutri841@gmail.com>
2025-05-23 23:21:51 +0200

    fixed bug in reservation factory

--- database/factories/ReservationFactory.php
+++ database/factories/ReservationFactory.php
@@ -23,32 +23,36 @@ class ReservationFactory extends Factory
         $table = $restaurant->tables()->inRandomOrder()->first();
 
         //take an opening date of the restaurant
-        $schedules = $restaurant->schedules()->get();
+        $schedules = $restaurant->restaurant_schedules()->get();
         $validSchedules = $schedules->filter(function ($s) {
             return !$s->is_lunch_closed || !$s->is_dinner_closed;
         });
 
         $schedule = $validSchedules->random();
         $reservationDate = Carbon::now()->addDays(rand(1, 30))->next($schedule->week_day);
-        
+
         // generate a random reservation time beetween openin and closing hour
-        if (!$schedule->is_lunch_closed) {
-            $timeSlot = Carbon::parse($schedule->lunch_opening)
-                ->addMinutes(rand(0, Carbon::parse($schedule->lunch_closing)->diffInMinutes($schedule->lunch_opening) - 1));
+        if (!$schedule->is_lunch_closed) { 
+            $opening = Carbon::parse($schedule->lunch_opening); 
+            $closing = Carbon::parse($schedule->lunch_closing); 
+            $minutesRange = $opening->diffInMinutes($closing); // calculate how many minutes are beetween opening and closing time
+            $timeSlot = $opening->copy()->addMinutes(rand(0, $minutesRange - 1)); // generate a random value in opening time
         } else {
-            $timeSlot = Carbon::parse($schedule->dinner_opening)
-                ->addMinutes(rand(0, Carbon::parse($schedule->dinner_closing)->diffInMinutes($schedule->dinner_opening) - 1));
+            $opening = Carbon::parse($schedule->dinner_opening); 
+            $closing = Carbon::parse($schedule->dinner_closing); 
+            $minutesRange = $opening->diffInMinutes($closing); // calculate how many minutes are beetween opening and closing time
+            $timeSlot = $opening->copy()->addMinutes(rand(0, $minutesRange - 1)); // generate a random value in opening time
         }
 
         return [
             'user_id' => User::where('role', '!=', 'owner')->inRandomOrder()->first()->id,
             'restaurant_id' => $restaurant->id,
             'table_id' => $table->id,
-            'guests_number'  => fake()->numberBetween(1, $table->seats ?? 4),
+            'guest_number'  => fake()->numberBetween(1, $table->seats ?? 4),
             'reservation_date' => $reservationDate->toDateString(),
             'reservation_time' => $timeSlot->format('H:i'),
             'status' =>  fake()->randomElement(['pending', 'confirmed', 'cancelled', 'completed']),
-            'notes' => $this->faker->optional()->sentence(),
+            'notes' => fake()->optional()->sentence(),
         ];
     }
 }
