96bd800a4fab5505d6c509c52e4098e0d17c828d
Antonio <antoniocutri841@gmail.com>
2025-05-27 23:34:03 +0200

    add a react example

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -12,11 +12,21 @@ function App() {
     return <h1>Hello from React!</h1>;
 }
 
+function Prova() {
+    return <h1>Hello from Prova!</h1>;
+}
+
 const rootElement = document.getElementById('root');
+const prova1 = document.getElementById('prova');
 
 if (rootElement) {
     const root = ReactDOM.createRoot(rootElement);
     root.render(<App />);
+}
+
+if (prova1) {
+    const p = ReactDOM.createRoot(prova1);
+    p.render(<Prova />)
 }
 
 root.render(<App />);
