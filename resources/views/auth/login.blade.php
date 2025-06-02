4049084c3f5fd5680bfd642b19c34bfd03a4cb14
Antonio <antoniocutri841@gmail.com>
2025-06-02 16:48:54 +0200

    add client-side validation to login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,14 +2,14 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
+    <form method="POST" id="login_form" action="{{ route('login') }}">
         @csrf
 
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" id="email_error" class="mt-2" />
         </div>
 
         <!-- Password -->
@@ -19,9 +19,9 @@
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
                             name="password"
-                            required autocomplete="current-password" />
+                            autocomplete="current-password" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <x-input-error :messages="$errors->get('password')" id="password_error" class="mt-2" />
         </div>
 
         <!-- Remember Me -->
@@ -43,10 +43,39 @@
                     </a>
                 @endif
 
-                <x-primary-button class="ms-3">
+                <x-primary-button class="ms-3" id="login_button">
                     {{ __('Log in') }}
                 </x-primary-button>
             </div>
         </div>
     </form>
 </x-guest-layout>
+<script>
+    let button = document.querySelector("#login_button");
+    let hasError = false
+    button.addEventListener("click", function (e) {
+
+        let email = document.querySelector("#email")
+        let password = document.querySelector("#password")
+        hasError = false 
+
+        if (!email.value) {
+            document.querySelector("#email_error").textContent = "Inserire l'email"
+            email.classList.add("is-invalid");
+            hasError = true
+        }
+
+        if (!password.value) {
+            document.querySelector("#password_error").textContent = "Inserire la password"
+            password.classList.add("is-invalid");
+            hasError = true
+        }
+        });
+
+    const form = document.querySelector("#login_form");
+    form.addEventListener("submit", function (e) {
+        if (hasError) {
+            e.preventDefault();
+        }
+    });
+</script>
