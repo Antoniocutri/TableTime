8ce2f57ed83fc11d1f9f152b40a3f2046e4a4d13
Antonio <antoniocutri841@gmail.com>
2025-07-27 21:52:18 +0200

    fixed translaction

--- resources/js/i18n.js
+++ resources/js/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import translationIT from 'C:/Users/Utente/Herd/tabletime/lang/it/restaurantSchedule.json';
+import translationIT from 'C:/Users/Utente/Herd/tabletime/lang/it.json';
 
 i18n
   .use(initReactI18next)
