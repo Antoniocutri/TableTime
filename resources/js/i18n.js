e8ed41ae7755fa3cab21fd2225a10deac8b41190
Antonio <antoniocutri841@gmail.com>
2025-07-27 19:47:09 +0200

    added i18n dependencies

---
+++ resources/js/i18n.js
@@ -0,0 +1,21 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import translationIT from 'C:/Users/Utente/Herd/tabletime/lang/it/restaurantSchedule.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      it: {
+        translation: translationIT,
+      },
+    },
+    lng: 'it',
+    fallbackLng: 'it',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
