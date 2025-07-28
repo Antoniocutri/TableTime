ed0afb8c817533180aad1cf2c228deca5a72adfb
Antonio <antoniocutri841@gmail.com>
2025-07-28 23:16:49 +0200

    create component Input time, Checkbox and error

--- resources/js/pages/restaurantSchedules.jsx
+++ resources/js/pages/restaurantSchedules.jsx
@@ -25,3 +25,56 @@ function Header(){
         </>
     )
 }
+
+const InputTime = ({ label, name, register, errors, validateFn }) => {
+
+    return (
+        <>
+            <Label label={label} name={name}/>
+            <input
+                {...register(name, {
+                    required: "Inserire l'orario",
+                    validate: validateFn
+                })}
+                type='time'
+                name={name}
+                id={name}
+                className="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-50 mt-1"
+            />
+            {errors[name] && <Error error={errors[name].message}/>}
+        </>
+    )
+}
+
+const CheckBox = ({label, name}) => {
+
+    return(
+        <>
+            <div className="form-check">
+                <label className="form-check-label font-medium text-sm text-gray-700" htmlFor={name}>
+                    {label}
+                </label>
+                <input 
+                    type='checkbox' 
+                    name={name}
+                    className="form-check-input"/>
+            </div>
+        </>
+    )
+}
+
+const Error = ({error}) =>{
+  return (
+    <p className='text-sm text-red-600 space-y-1'>{error}</p>
+  )
+}
+
+const Label = ({label, name}) => {
+    return (
+        <>
+            <label htmlFor={name} className="block font-medium text-sm text-gray-700">
+                {label}
+            </label>
+        </>
+    )
+}
