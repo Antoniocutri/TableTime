a5860d69b742d59667f0d04f51d6ee146b6c7f87
Antonio <antoniocutri841@gmail.com>
2025-08-02 23:00:04 +0200

    add creation of restaurant schedule in store method

--- app/Http/Controllers/RestaurantScheduleController.php
+++ app/Http/Controllers/RestaurantScheduleController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RestaurantScheduleRequest;
+use App\Models\Restaurant_schedule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redirect;
 
 class RestaurantScheduleController extends Controller
 {
@@ -28,7 +31,20 @@ class RestaurantScheduleController extends Controller
      */
     public function store(RestaurantScheduleRequest $request)
     {
-        return $request->validated();
+        $request->validated();
+
+        Restaurant_schedule::create([
+            'restaurant_id' => $request->user()->restaurants[0]->id,
+            'week_day' => $request->week_day,
+            'is_lunch_closed' => $request->isLunch_closed,
+            'lunch_opening' => $request->lunch_opening,
+            'lunch_closing' => $request->lunch_closing,
+            'is_dinner_closed' => $request->isDinner_closed,
+            'dinner_opening' => $request->dinner_opening,
+            'dinner_closing' => $request->dinner_closing,
+        ]);
+
+        return Redirect::route('profile.edit')->with('status', 'schedule-stored');
     }
 
     /**
