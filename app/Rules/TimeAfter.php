e08b977c149825fa56ad1b4595dd82e0b48a17da
Antonio <antoniocutri841@gmail.com>
2025-08-02 22:20:28 +0200

    modified time after rule

--- app/Rules/TimeAfter.php
+++ app/Rules/TimeAfter.php
@@ -10,12 +10,10 @@ class TimeAfter implements ValidationRule
 {
     protected $startTimeField;
     protected $startTimeValue;
-    protected $isClosed;
 
-    public function __construct($startTimeField, $isClosed)
+    public function __construct($startTimeField)
     {
         $this->startTimeField = $startTimeField;
-        $this->isClosed = $isClosed;
     }
 
     /**
@@ -24,26 +22,14 @@ class TimeAfter implements ValidationRule
      * @param  \Closure(string, ?string=): \Illuminate\Translation\PotentiallyTranslatedString  $fail
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
-    {
-        Log::info(request()->boolean($this->isClosed));
-        // ignore rules if is closed is true
-        if ($this->isClosed && request()->boolean($this->isClosed)) {
-            Log::alert('heree');
-            return; 
-        }
-
+    {   
         // Take the start value from request
         $this->startTimeValue = request()->input($this->startTimeField);
 
         $start = \DateTime::createFromFormat('H:i', $this->startTimeValue);
         $end = \DateTime::createFromFormat('H:i', $value);
 
-        if (!$start || !$end) {
-            Log::info('chiuso');
-            return;
-        }
-
-        if($end > $start){
+        if($end < $start){
             $fail(__('Closing must be later than opening'));
         };
     }
